7a83fc758df985bfec37e24d758767b17fbd7f02
Ayushm010 <mohapatraayush587@gmail.com>
2025-09-07 02:21:28 +0530

    feat: basic structure of image carousel added

--- src/index.js
+++ src/index.js
@@ -1,13 +1,14 @@
 import "./style.css";
 
-// Dropdown list
+// Dropdown Menu
 
-const dropdownElement = document.querySelector(".dropdown");
+const dropdownElement = document.querySelector(".dropdown-text");
 
 dropdownElement.addEventListener("click", () => {
   const dropdownMenu = document.querySelector(".dropdown-menu"); // Selects the dropdown menu the parent div for items
 
-  if (dropdownMenu.classList.contains("menu-visible")) { // for toggling 
+  if (dropdownMenu.classList.contains("menu-visible")) {
+    // for toggling
     console.log("here");
     dropdownMenu.classList.remove("menu-visible");
 
@@ -27,3 +28,15 @@ dropdownElement.addEventListener("click", () => {
     });
   }
 });
+
+// Image Carousel
+
+import brbaIMg from "./img/BrBa.jpeg";
+import darkImg from "./img/Dark.jpeg";
+import gotImg from "./img/Got.jpeg";
+import trueDetectiveImg from "./img/TrueDetective.jpeg";
+import daredevilImg from "./img/Daredevil.jpeg";
+
+let imgSource = [brbaIMg, darkImg, gotImg, trueDetectiveImg, daredevilImg];
+
+
